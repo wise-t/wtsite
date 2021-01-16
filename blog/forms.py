7861e75a59fecd49297c246ee2b20370aa941960
wise-t <lovjes4@gmail.com>
2021-01-16 11:40:27 +0000

    formwt3

--- blog/forms.py
+++ blog/forms.py
@@ -1,37 +1,36 @@
 from django import forms
-from .models import  Post,Category
-choices=[('sport','sport'),('coding','coding'),('entertaiment','entertaiment')]
-choices=Category.objects.all().values_list('name','name')
+from .models import Post, Category#, Comment
+#choices=[('sport','sport'),('coding','coding'),('entertaiment','entertaiment')]
+choices = Category.objects.all().values_list('name', 'name')
 
-choices_list=[]
+choices_list = []
 for item in choices:
     choices_list.append(item)
 
 
-
-
 class PostForm(forms.ModelForm):
     class Meta:
-        model=Post
-        fields=('title','title_tag','author','category','body')
-        widgets={
-            'title':forms.TextInput(attrs={'class':'form-control'}),
-            'title_tag':forms.TextInput(attrs={'class':'form-control'}),
+        model = Post
+        fields = ('title', 'title_tag', 'author', 'category', 'body')
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
 
-            'author':forms.TextInput(attrs={'class':'form-control','value':'','id':'elder','type':'hidden'}),
+            'author': forms.TextInput(attrs={'class': 'form-control', 'value': '', 'id': 'elder', 'type': 'hidden'}),
 
             #'author':forms.Select(attrs={'class':'form-control'}),
-            'category':forms.Select(choices=choices_list,attrs={'class':'form-control'}),
-            'body':forms.Textarea(attrs={'class':'form-control'}),
+            'category': forms.Select(choices=choices_list, attrs={'class': 'form-control'}),
+            'body': forms.Textarea(attrs={'class': 'form-control'}),
         }
+
 
 class EditForm(forms.ModelForm):
     class Meta:
-        model=Post
-        fields=('title','title_tag','body')
-        widgets={
-            'title':forms.TextInput(attrs={'class':'form-control'}),
-            'title_tag':forms.TextInput(attrs={'class':'form-control'}),
+        model = Post
+        fields = ('title', 'title_tag', 'body')
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
             #'author':forms.Select(attrs={'class':'form-control'}),
-            'body':forms.Textarea(attrs={'class':'form-control'}),
+            'body': forms.Textarea(attrs={'class': 'form-control'}),
         }
